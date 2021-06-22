a4f6ee41d466bc93306c2657940e17d8f9483875
aaaafr <anastasia.frassini@icloud.com>
2021-06-22 08:58:10 +0200

    complete

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ $string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, maiores.';
+
+// var_dump($_GET);
+$badword = $_GET['bad'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+<p> <?php echo $string; ?> <?php echo strlen($string);?></p>
+
+    <p> <?php echo str_replace('ulo','***' , $badword); ?> </p>
+</body>
+</html>
